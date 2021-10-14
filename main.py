6aa39b3c54b1b648c8181903a21a105e114dc05f
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2021-10-14 10:15:52 +0700

    membuat main.py, menambahkan author, version, dan contact

---
+++ main.py
@@ -0,0 +1,18 @@
+#main.py
+#encoding: utf-8
+
+__author__      = "Muhamad Zulfan Taqiyudin Baehaki", "Muhammad Rafif Fadlurahman"
+__version__     = 0.1
+__contact__     = {
+                    "Rafif" :{
+                        "Name"          :   "Muhammad Rafif Fadlurahman",
+                        "Email"         :   "rafif.fadlurahman20@mhs.uinjkt.ac.id",
+                        "University"    :   "UIN SYARI FHIDAYATULLAH JAKARTA"
+
+                    },
+                    "Zulfan":{
+                        "Name"          :   "Muhamad Zulfan Taqiyudin Baehaki",
+                        "Email"         :   "muhamad.zulfan20@mhs.uinjkt.ac.id",
+                        "University"    :   "UIN SYARIF HIDAYATULLAH JAKARTA"
+                    }    
+} 
